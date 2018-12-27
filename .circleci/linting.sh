b0e8b2761473705b3ba366c6d27597b587cccb44
Florent Clairambault <florent@habx.fr>
2018-12-27 02:08:43 +0100

    test

--- .circleci/linting.sh
+++ .circleci/linting.sh
@@ -4,4 +4,4 @@ go list -f '{{.Dir}}' ./...
 
 curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.12.5
 
-golangci-lint run -v  ./... # --enable-all -D lll
+golangci-lint run ./... # --enable-all -D lll
