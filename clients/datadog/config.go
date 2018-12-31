41cb40c997babca056c6ac6843f20d754d314f01
Florent Clairambault <florent@clairambault.fr>
2018-12-31 11:53:28 +0100

    README update (#3)
    * Updating readme
    
    * ...er

--- clients/datadog/config.go
+++ clients/datadog/config.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
+// Config is the datadog output client config
 type Config struct {
 	Token                    string            `envconfig:"DATADOG_TOKEN"`               // Datadog token
 	Server                   string            `envconfig:"DATADOG_SERVER"`              // Datadog server
@@ -14,10 +15,11 @@ type Config struct {
 	KeysToTagsConversions    map[string]string `envconfig:"DATADOG_FIELDS_CONV_TAGS"`    // Logstash to session fields conversion
 }
 
+// NewConfig creates a new config instance
 func NewConfig() *Config {
 	return &Config{
 		// "tcp-intake.logs.datadoghq.eu:443" for europe
-		Server:    "intake.logs.datadoghq.com",
+		Server:    "intake.logs.datadoghq.com:15516",
 		QueueSize: 20,
 		KeysToMessageConversions: map[string]string{
 			"appname": "service",
@@ -29,6 +31,7 @@ func NewConfig() *Config {
 	}
 }
 
+// Load performs the config loading
 func (c *Config) Load() error {
 	if err := envconfig.Process("", c); err != nil {
 		return fmt.Errorf("couldn't load config from env vars: %s", err)
@@ -39,13 +42,11 @@ func (c *Config) Load() error {
 	return nil
 }
 
+// Enabled reports if the client should be used
 func (c *Config) Enabled() bool {
 	return c.Token != ""
 }
 
 func (c *Config) check() error {
-	if c.Token == "" {
-		return fmt.Errorf("no datadog token was provided, use DATADOG_TOKEN env var to specify it")
-	}
 	return nil
 }
