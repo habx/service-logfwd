e9d1a9d62e317fc2ba1aed2228809dce99c3ce93
Florent Clairambault <florent@habx.fr>
2019-01-02 00:25:27 +0100

    test

--- .circleci/build-binary.sh
+++ .circleci/build-binary.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -ex
 
+if [ -f version.txt ]; then
+  version=$(cat version.txt)
+  echo "package main
+
+const VERSION=\"$version\"
+" >version.go
+fi
+
 go get -v
 CGO_ENABLED=0 go build -o logfwd
 
