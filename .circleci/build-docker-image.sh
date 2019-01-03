1b70395d2292d3eefa6a7e0fedc43d99df672d92
I'm a robot <39249053+habxtech@users.noreply.github.com>
2019-01-04 00:29:22 +0100

    Bringing correct tagging process
    Both on the git and docker side

--- .circleci/build-docker-image.sh
+++ .circleci/build-docker-image.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -ex
+# Test it with:
+# docker run -ti --rm -v $(pwd):/work -v /var/run/docker.sock:/var/run/docker.sock -w /work docker:18
 
 if [ ! -f logfwd ]; then
     cp /workspace/logfwd .
@@ -6,23 +8,23 @@ fi
 
 DOCKER_IMAGE_NAME=habx/logfwd
 
-if [ "${CIRCLE_TAG}" != "" ]; then
-    DOCKER_IMAGE_TAG=${CIRCLE_TAG}
-elif [ "${CIRCLE_BRANCH}" != "" ]; then
-    DOCKER_IMAGE_TAG=${CIRCLE_BRANCH}
+if [ "${CIRCLE_TAG}" != "" ]; then # Tag names are cleaned up and simplified
+    # Am I going to hell ?
+    DOCKER_IMAGE_TAGS=$(echo ${CIRCLE_TAG} | grep -Eo "v([0-9]+\.){2}[0-9]+")' '$(echo ${CIRCLE_TAG} | grep -Eo "v([0-9]+\.){1}[0-9]+")' '$(echo ${CIRCLE_TAG} | grep -Eo "v([0-9]+\.){0}[0-9]+")
+elif [ "${CIRCLE_BRANCH}" != "" ]; then # Branch names are cleaned up
+    DOCKER_IMAGE_TAGS=$(echo ${CIRCLE_BRANCH} | sed "s/[^a-zA-Z0-9\.]/-/g")
 else
     DOCKER_IMAGE_TAG=test
 fi
 
-# Cleaning up image name
-DOCKER_IMAGE_TAG=$(echo ${DOCKER_IMAGE_TAG} | sed "s/[^a-zA-Z0-9-]/_/g")
-
-DOCKER_IMAGE_FULL=${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
-
-docker build . -t ${DOCKER_IMAGE_FULL}
+docker build . -t ${DOCKER_IMAGE_NAME}:local
 
 set +x # WARNING - SECRET
 echo ${DOCKER_AUTH_PASS} | docker login -u ${DOCKER_AUTH_USER} --password-stdin
 set -x
 
-docker push ${DOCKER_IMAGE_FULL}
+for tag in $DOCKER_IMAGE_TAGS
+do
+  docker tag ${DOCKER_IMAGE_NAME}:local ${DOCKER_IMAGE_NAME}:${tag}
+  docker push ${DOCKER_IMAGE_NAME}:${tag}
+done
