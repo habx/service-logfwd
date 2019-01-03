9de5c6e9a5be95fd927e2b7bada46234eba6e676
Florent Clairambault <florent@habx.fr>
2019-01-04 00:17:18 +0100

    Adding back auth

--- .circleci/build-docker-image.sh
+++ .circleci/build-docker-image.sh
@@ -12,16 +12,16 @@ if [ "${CIRCLE_TAG}" != "" ]; then # Tag names are cleaned up and simplified
     # Am I going to hell ?
     DOCKER_IMAGE_TAGS=$(echo ${CIRCLE_TAG} | grep -Eo "v([0-9]+\.){0}[0-9]+")' '$(echo ${CIRCLE_TAG} | grep -Eo "v([0-9]+\.){1}[0-9]+")' '$(echo ${CIRCLE_TAG} | grep -Eo "v([0-9]+\.){2}[0-9]+")
 elif [ "${CIRCLE_BRANCH}" != "" ]; then # Branch names are cleaned up
-    DOCKER_IMAGE_TAGS=$(echo ${CIRCLE_BRANCH} | sed "s/[^a-zA-Z0-9\.-]/_/g")
+    DOCKER_IMAGE_TAGS=$(echo ${CIRCLE_BRANCH} | sed "s/[^a-zA-Z0-9\.]/-/g")
 else
     DOCKER_IMAGE_TAG=test
 fi
 
 docker build . -t ${DOCKER_IMAGE_NAME}:local
 
-# set +x # WARNING - SECRET
-# echo ${DOCKER_AUTH_PASS} | docker login -u ${DOCKER_AUTH_USER} --password-stdin
-# set -x
+set +x # WARNING - SECRET
+echo ${DOCKER_AUTH_PASS} | docker login -u ${DOCKER_AUTH_USER} --password-stdin
+set -x
 
 for tag in $DOCKER_IMAGE_TAGS
 do
